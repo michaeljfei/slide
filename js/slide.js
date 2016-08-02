650f5ebc605dfa042e1beccd89a056e54197772e
MICHAEL <michaeljfei@163.com>
2016-08-03 01:22:53 +0800

    fix some bug

--- js/slide.js
+++ js/slide.js
@@ -1,26 +1,36 @@
 $(function(){
 	var slide = {
+		i: 0,
 		start : 0,
 		current : 0,
-		time : 1000,
-		itemList: $('.slide-item'),
+		time : 2000,
+		itemList : $('.slide-item'),
 		ctrlList : $('.ctrl-item'),
 		timeout : null,
-		
 		len : function(){
 			return this.itemList.length;
 		},
 		init : function(){
-			this.changeItem();
+			var $this = this;
+			this.timeout = setTimeout(function(){
+				$this.auto();
+			});
 			this.bind();
 		},
-		auto : function(func){
+		auto : function(){
+			var $this = this;
 			clearTimeout(this.timeout);
-			timeout = setTimeout(func.call(this),this.time);
-		}
-		,
+			this.animate(this.itemList.eq(this.current));
+			this.current = (this.current + 1) % this.len();
+			this.timeout = setTimeout(function(){
+				$this.auto();
+			},this.time);
+		},
 		changeItem : function(){
 			this.itemList.eq(this.current).addClass('slide-item-active');
+		},
+		changeCtrl : function(){
+			this.ctrlList.eq(this.current).addClass('ctrl-active');
 		},
 		removeItem : function(){
 			this.itemList.each(function(){
@@ -33,18 +43,19 @@ $(function(){
 				});
 		},
 		animate : function(){
-
+			this.removeCtrl();
+			this.removeItem();
+			this.changeCtrl();
+			this.changeItem();
 		},
 		bind : function(){
 			var $this = this;
 			this.ctrlList.click(function(){
-				$this.removeCtrl();
-				$this.removeItem();
-				$(this).addClass('ctrl-active');
 				$this.current = $this.ctrlList.index($(this));
-				$this.changeItem();
+				$this.animate($(this));
 			})
 		}
 	}
+
 	slide.init();
 });
